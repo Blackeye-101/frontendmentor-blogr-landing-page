fb880f9800203400e170f2fb6a166fdcf80d376d
Rishabh Thakur <73433485+Blackeye-101@users.noreply.github.com>
2021-06-11 23:12:47 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+  var toggled = false;
+  $(".menu-toggle").click(function () {
+    toggled = !toggled;
+    $("nav").toggleClass("active");
+    if (toggled) {
+      $("#hambrgr").attr("src", "images/icon-close.svg");
+    } else {
+      $("#hambrgr").attr("src", "images/icon-hamburger.svg");
+    }
+  });
+
+  $("ul li").click(function () {
+    $(this).siblings().removeClass("active");
+    $(this).toggleClass("active");
+  });
+
+  $(window).resize(function (e) {
+    if ($(window).width() < 990) {
+      $("#editor-image").each(function () {
+        $(this).attr("src", "images/illustration-editor-mobile.svg");
+      });
+    } else {
+      $("#editor-image").each(function () {
+        $(this).attr("src", "images/illustration-editor-desktop.svg");
+      });
+    }
+    if ($(window).width() < 990) {
+      $("#laptop-image").each(function () {
+        $(this).attr("src", "images/illustration-laptop-mobile.svg");
+      });
+    } else {
+      $("#laptop-image").each(function () {
+        $(this).attr("src", "images/illustration-laptop-desktop.svg");
+      });
+    }
+  });
+});
